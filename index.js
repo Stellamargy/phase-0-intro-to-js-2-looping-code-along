3474eb2def0a2084d2dedfca869531265b965125
Stellamargy <stella.oloo@student.moringaschool.com>
2022-11-27 12:04:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+function writeCards(arrayofnames, eventname) {
+    let birthdayCards = []
+
+    for (let i = 0; i < arrayofnames.length; i++ ) {
+     birthdayCards.push(`Thank you, ${arrayofnames[i]}, for the wonderful ${eventname} gift!`)
+    }
+    return birthdayCards
+}
+
+function countDown(providedNumber) {
+    while  (providedNumber > 0) {
+        console.log( providedNumber);
+        providedNumber -= 1;
+    }
+    console.log(providedNumber)
+}
